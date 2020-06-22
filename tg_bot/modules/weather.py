fcb636d80e00a86ee3865a4554ceebb2f008440a
mrsammy07 <66665117+mrsammy07@users.noreply.github.com>
2020-06-22 14:15:51 +0530

    Update weather.py

--- tg_bot/modules/weather.py
+++ tg_bot/modules/weather.py
@@ -65,7 +65,7 @@ __help__ = """
  - /weather <city>: get weather info in a particular place
 """
 
-__mod_name__ = "WEATHER"
+__mod_name__ = "🌦️ Weather"
 
 WEATHER_HANDLER = DisableAbleCommandHandler("weather", weather, pass_args=True)
 
